7813f66348475f002f89e24875612f9b35228eee
HwangJaeHyuk <jhhwang.2018@sis.smu.edu.sg>
2023-12-11 23:04:18 +0800

    first push
    Added:
    - various starting folders required for frontend development
    
    Bugs/Comments:
    - Currently does not contain any add-ons.
    - Blank testing grounds

---
+++ chatbot_front-end/src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Verbot",
+  description: "Verbot by Vertex",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ chatbot_front-end/src/app/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>This is the testing server</div>;
+};
+
+export default page;
