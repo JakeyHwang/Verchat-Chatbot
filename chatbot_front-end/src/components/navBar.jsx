a137eb663336917c67f0140903c4460cb2ded55b
HwangJaeHyuk <jhhwang.2018@sis.smu.edu.sg>
2023-12-20 16:06:40 +0800

    Update navBar.jsx
    added:
    - Navbar is now responsive to window size and will remove the nav items when window is small.
    - hamburger icon will now be visible in small windows: pressing it will allow the nav items to appear

--- chatbot_front-end/src/components/navBar.jsx
+++ chatbot_front-end/src/components/navBar.jsx
@@ -6,21 +6,23 @@ const Navbar = () => {
   const [menu, setMenu] = useState(false);
 
   return (
-    <nav className="bg-blue-400 flex flex-row justify-between items-center text-2xl font-bold sticky top-0">
-      <Link href="/" className="justify-start m-2">
-        Verbot
-      </Link>
-      {/* <div
-        className="max-sm:flex hidden absolute top-3 right-4 mr-3 flex-col justify-between w-9 h-8"
-        onClick={() => setMenu(!menu)}
-      >
-        <span className="h-2 w-full bg-white rounded"></span>
-        <span className="h-2 w-full bg-white rounded"></span>
-        <span className="h-2 w-full bg-white rounded"></span>
-      </div> */}
+    <nav className="bg-blue-400 flex flex-row justify-between items-center text-2xl font-bold sticky top-0 max-sm:flex-col">
+      <div className="flex flex-col">
+        <Link href="/" className="justify-start m-2">
+          Verbot
+        </Link>
+        <div
+          className="max-sm:flex hidden absolute top-2 right-0 mr-3 flex-col justify-between w-9 h-8"
+          onClick={() => setMenu(!menu)}
+        >
+          <span className="h-2 w-full bg-white rounded"></span>
+          <span className="h-2 w-full bg-white rounded"></span>
+          <span className="h-2 w-full bg-white rounded"></span>
+        </div>
+      </div>
 
-      <div className="max-sm:hidden max-sm:w-full max-sm:mb-1">
-        <ul className="flex max-sm:flex-col ">
+      <div className={menu ? "" : "max-sm:hidden"}>
+        <ul className="flex max-sm:flex-col max-sm:w-full max-sm:mb-1">
           <li className="list-none max-sm:w-full max-sm:text-center">
             <Link
               className="max-sm:my-1 max-sm:mx-2 p-2 mx-2 active:bg-blue-600 hover:bg-blue-200 rounded"
