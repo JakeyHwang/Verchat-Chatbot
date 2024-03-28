df942de0792fd8958b5dbcaded1f35d3a0815229
HwangJaeHyuk <jhhwang.2018@sis.smu.edu.sg>
2024-03-28 21:30:35 +0800

    added new code for pdf reader
    PDF reader can now take in multiple PDFs and give correct and accurate responses

--- chatbot_back-end/app/PDF_loader.py
+++ chatbot_back-end/app/PDF_loader.py
@@ -25,7 +25,7 @@ for file in os.listdir('company_data'):
 # we split the data into chunks of 1,000 characters, with an overlap
 # of 200 characters between the chunks, which helps to give better results
 # and contain the context of the information between chunks
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
 documents = text_splitter.split_documents(documents)
 
 # we create our vectorDB, using the OpenAIEmbeddings tranformer to create
@@ -44,20 +44,19 @@ qa_chain = ConversationalRetrievalChain.from_llm(
     verbose=False
 )
 
+yellow = "\033[0;33m"
+green = "\033[0;32m"
+
 chat_history = []
+
 while True:
-    # this prints to the terminal, and waits to accept an input from the user
-    query = input('Prompt: ')
-    # give us a way to exit the script
-    if query == "exit" or query == "quit" or query == "q":
+    query = input(f"{green}Prompt: ")
+    if query == "exit" or query == "quit" or query == "q" or query == "f":
         print('Exiting')
         sys.exit()
-    # we pass in the query to the LLM, and print out the response. As well as
-    # our query, the context of semantically relevant information from our
-    # vector store will be passed in, as well as list of our chat history
-    result = qa_chain({'question': query, 'chat_history': chat_history})
-    print('Answer: ' + result['answer'])
-    # we build up the chat_history list, based on our question and response
-    # from the LLM, and the script then returns to the start of the loop
-    # and is again ready to accept user input.
-    chat_history.append((query, result['answer']))
+    if query == '':
+        continue
+    result = qa_chain.invoke(
+        {"question": query, "chat_history": chat_history})
+    print(f"{yellow}Answer: " + result["answer"])
+    chat_history.append((query, result["answer"]))
